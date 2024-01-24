1ef8f00b2312af402206cfea90c4210b9f003f9b
BhupeshB7 <bhupeshkr2912@gmail.com>
2024-01-24 21:27:48 +0530

    update

--- server.js
+++ server.js
@@ -11,22 +11,29 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // useFindAndModify: false, 
-});
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   // useFindAndModify: false, 
+// });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
+// db.on('error', (err) => {
+//   console.error('MongoDB connection error:', err);
+// });
 
-db.once('open', () => {
-  console.log('MongoDB connected successfully');
-});
-
+// db.once('open', () => {
+//   console.log('MongoDB connected successfully');
+// });
+// Connect to MongoDB database
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error(error));
 app.use(cors());
 app.use(express.json());
 
